f5b7cfcfc8f51e524daf375e763a6c4d1312b159
James Gu <jamesgu521@gmail.com>
2021-01-13 17:28:53 -0600

    Nest Urselks into Urselks Hall and implement SCP

---
+++ Levels/Level/Behaviors/Script_LevelBehavior_39.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Script_LevelBehavior_39 : Script_LevelBehavior
+{
+    /* =======================================================================
+        STATE DATA
+    ======================================================================= */
+
+    /* ======================================================================= */
+    
+    [SerializeField] private Script_DemonNPC Flan;
+
+    // Need for Guard OnTrigger Cut Scene
+    protected override void HandleAction()
+    {
+        base.HandleDialogueAction();
+    }
+
+    // ----------------------------------------------------------------------
+    // Unity Events START
+    
+    // called from Trigger in front of Guard
+    public void StartGuardDialogue()
+    {
+        Debug.Log("Triggering Flan convo");
+        game.ChangeStateCutScene();
+        Flan.TriggerDialogue();
+
+        /// NOTE Flan's last dialogue node needs to call EndGuardDialogue()
+    }
+
+    public void OnEndGuardDialogue()
+    {
+        game.ChangeStateInteract();
+    }
+
+    // Unity Events END
+    // ----------------------------------------------------------------------
+
+    public override void Setup()
+    {
+    }        
+}
+
+#if UNITY_EDITOR
+#endif
---
+++ Objects/Interactables/InteractableObjects/ScarletCipherPiece/Script_ScarletCipherPiece.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_ScarletCipherPiece : Script_InteractableObject
+{
+    private int _scarletCipherId;
+    
+    private int ScarletCipherId
+    {
+        get => _scarletCipherId;
+        set => _scarletCipherId = value;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        // Singletons hooked up in Awake() 
+        if (Script_ScarletCipherManager.Control.ScarletCipherVisibility[ScarletCipherId])
+        {
+            Hide();
+        }
+    }
+
+    public override void ActionDefault()
+    {
+        Script_ScarletCipherManager.Control.RevealScarletCipherSlot(ScarletCipherId);
+        Hide();
+    }
+
+    private void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
