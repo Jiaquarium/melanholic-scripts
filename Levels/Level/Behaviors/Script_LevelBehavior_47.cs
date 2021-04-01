b7bc173d8490726034bdbdce9292a8e8089f2482
James Gu <jamesgu521@gmail.com>
2021-04-01 13:28:26 -0500

    Place Puppeteer sticker in Sanctuary and add to state

--- Levels/Level/Behaviors/Script_LevelBehavior_47.cs
+++ Levels/Level/Behaviors/Script_LevelBehavior_47.cs
@@ -10,7 +10,35 @@ public class Script_LevelBehavior_47 : Script_LevelBehavior
 {
     // ==================================================================
     // State Data
-    
+    public bool didPickUpPuppeteerSticker;
     // ==================================================================
-        
+    
+    [SerializeField] private Script_StickerObject puppeteerSticker;
+
+    protected override void OnEnable()
+    {
+        Script_ItemsEventsManager.OnItemPickUp += OnItemPickUp;
+    }
+
+    protected override void OnDisable()
+    {
+        Script_ItemsEventsManager.OnItemPickUp -= OnItemPickUp;
+    }
+    
+    private void OnItemPickUp(string itemId)
+    {
+        if (itemId == puppeteerSticker.Item.id)
+        {
+            didPickUpPuppeteerSticker = true;
+        }
+    }
+
+    public override void Setup()
+    {
+        if (puppeteerSticker != null)
+        {
+            if (didPickUpPuppeteerSticker)  puppeteerSticker.gameObject.SetActive(false);
+            else                            puppeteerSticker.gameObject.SetActive(true);
+        }
+    }
 }
---
+++ SaveLoad/SaveHandlers/GameInfo/Levels/LevelBehaviors/Script_SaveLoadLevelBehavior_47.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_SaveLoadLevelBehavior_47 : Script_SaveLoadLevelBehavior
+{
+    [SerializeField] private Script_LevelBehavior_47 LB47;
+
+    public override void Save(Model_RunData data)
+    {
+        Model_LevelBehavior_47 lvlModel = new Model_LevelBehavior_47(
+            _didPickUpPuppeteerSticker               : LB47.didPickUpPuppeteerSticker
+        );
+        
+        data.levelsData.LB47 = lvlModel;
+    }
+
+    public override void Load(Model_RunData data)
+    {
+        Model_LevelBehavior_47 lvlModel         = data.levelsData.LB47;
+        
+        LB47.didPickUpPuppeteerSticker                   = lvlModel.didPickUpPuppeteerSticker;
+
+        Debug.Log($"-------- LOADED {name} --------");
+        Script_Utils.DebugToConsole(lvlModel);
+    }
+}
