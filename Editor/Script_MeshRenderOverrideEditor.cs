5537be893d48b159d0b46e08f5efe87e1c910143
James Gu <jamesgu521@gmail.com>
2021-05-05 16:09:09 -0500

    Remove mesh renderer custom editor.
    Code is useless because we're now using Z depth for Meshes after implementing the Vertical ZDepth Sprite Shader

--- Editor/Script_MeshRenderOverrideEditor.cs
+++ Editor/Script_MeshRenderOverrideEditor.cs
@@ -6,51 +6,51 @@ using System;
 using System.Collections;
 using System.Reflection;
 
-[CustomEditor(typeof(MeshRenderer))]
-public class MeshRenderOverrideEditor : Editor
-{
-    MeshRenderer renderer
-    {
-        get
-        {
-            return target as MeshRenderer;
-        }
-    }
+// [CustomEditor(typeof(MeshRenderer))]
+// public class MeshRenderOverrideEditor : Editor
+// {
+//     MeshRenderer renderer
+//     {
+//         get
+//         {
+//             return target as MeshRenderer;
+//         }
+//     }
 
-    public override void OnInspectorGUI()
-    {
-        EditorGUILayout.HelpBox("Custom Inspector", MessageType.Info);
+//     public override void OnInspectorGUI()
+//     {
+        // EditorGUILayout.HelpBox("Custom Inspector", MessageType.Info);
 
-        EditorGUI.BeginChangeCheck();
+        // EditorGUI.BeginChangeCheck();
 
-        var so = new SerializedObject(renderer);
+        // var so = new SerializedObject(renderer);
 
-        EditorGUILayout.PropertyField(so.FindProperty("m_CastShadows"));
-        EditorGUILayout.PropertyField(so.FindProperty("m_ReceiveShadows"));
-        EditorGUILayout.PropertyField(so.FindProperty("m_MotionVectors"));
-        EditorGUILayout.PropertyField(so.FindProperty("m_Materials"), true);
+        // EditorGUILayout.PropertyField(so.FindProperty("m_CastShadows"));
+        // EditorGUILayout.PropertyField(so.FindProperty("m_ReceiveShadows"));
+        // EditorGUILayout.PropertyField(so.FindProperty("m_MotionVectors"));
+        // EditorGUILayout.PropertyField(so.FindProperty("m_Materials"), true);
 
-        var options = GetSortingLayerNames();
-        var picks = new int[options.Length];
-        var name = renderer.sortingLayerName;
-        var choice = -1;
-        for (int i = 0; i < options.Length; i++)
-        {
-            picks[i] = i;
-            if (name == options[i]) choice = i;
-        }
+        // var options = GetSortingLayerNames();
+        // var picks = new int[options.Length];
+        // var name = renderer.sortingLayerName;
+        // var choice = -1;
+        // for (int i = 0; i < options.Length; i++)
+        // {
+        //     picks[i] = i;
+        //     if (name == options[i]) choice = i;
+        // }
 
-        choice = EditorGUILayout.IntPopup("Sorting Layer", choice, options, picks);
-        renderer.sortingLayerName = options[choice];
-        renderer.sortingOrder = EditorGUILayout.IntField("Sorting Order", renderer.sortingOrder);
+        // choice = EditorGUILayout.IntPopup("Sorting Layer", choice, options, picks);
+        // renderer.sortingLayerName = options[choice];
+        // renderer.sortingOrder = EditorGUILayout.IntField("Sorting Order", renderer.sortingOrder);
 
-        renderer.lightProbeUsage = (LightProbeUsage)EditorGUILayout.EnumPopup("Ligth Probes", renderer.lightProbeUsage);
-        renderer.reflectionProbeUsage = (ReflectionProbeUsage)EditorGUILayout.EnumPopup("Reflection Probes", renderer.reflectionProbeUsage);
-        renderer.probeAnchor = EditorGUILayout.ObjectField("Anchor Override", renderer.probeAnchor, typeof(Transform), true) as Transform;
+        // renderer.lightProbeUsage = (LightProbeUsage)EditorGUILayout.EnumPopup("Ligth Probes", renderer.lightProbeUsage);
+        // renderer.reflectionProbeUsage = (ReflectionProbeUsage)EditorGUILayout.EnumPopup("Reflection Probes", renderer.reflectionProbeUsage);
+        // renderer.probeAnchor = EditorGUILayout.ObjectField("Anchor Override", renderer.probeAnchor, typeof(Transform), true) as Transform;
 
-        if (EditorGUI.EndChangeCheck())
-            SceneView.RepaintAll();
-    }
+        // if (EditorGUI.EndChangeCheck())
+        //     SceneView.RepaintAll();
+    // }
 
     //void OnSceneGUI()
     //{
@@ -65,10 +65,10 @@ public class MeshRenderOverrideEditor : Editor
     //    }
     //}
 
-    public string[] GetSortingLayerNames()
-    {
-        Type internalEditorUtilityType = typeof(InternalEditorUtility);
-        PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
-        return (string[])sortingLayersProperty.GetValue(null, new object[0]);
-    }
-}
+//     public string[] GetSortingLayerNames()
+//     {
+//         Type internalEditorUtilityType = typeof(InternalEditorUtility);
+//         PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+//         return (string[])sortingLayersProperty.GetValue(null, new object[0]);
+//     }
+// }
