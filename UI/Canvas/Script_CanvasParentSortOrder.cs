b6fafce2e5b9f8bbaed4b07bf578cc51307a5f79
James Gu <jamesgu521@gmail.com>
2021-08-03 14:21:51 -0500

    Set sorting order of all dialogue & input & choice canvases via parent

---
+++ UI/Canvas/Script_CanvasParentSortOrder.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_CanvasParentSortOrder : MonoBehaviour
+{
+    /// <summary>
+    /// Set sorting order of all children canvases.
+    /// </summary>
+    [SerializeField] private int sortingOrder;
+    
+    void OnValidate()
+    {
+        SetChildrenCanvasesSortingOrder();
+    }
+
+    private void SetChildrenCanvasesSortingOrder()
+    {
+        Canvas[] children = GetComponentsInChildren<Canvas>(true);
+
+        foreach (Canvas c in children)
+        {
+            c.sortingOrder = sortingOrder;
+        }
+    }
+}
