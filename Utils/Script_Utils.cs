7f3d85e882ef9f328a6a846bc89d148ba6e9552d
James Gu <jamesgu521@gmail.com>
2021-09-20 18:22:22 -0500

    Fix bug in tag ignoring in collisions

--- Utils/Script_Utils.cs
+++ Utils/Script_Utils.cs
@@ -724,6 +724,8 @@ public static class Script_Utils
 
     public static bool CheckInTags(this List<Const_Tags.Tags> tags, string tag)
     {
+        Debug.Log($"Checking for tag {tag}");
+
         if (tag == null)            return false;
 
         // Check if Everything is included
@@ -731,6 +733,8 @@ public static class Script_Utils
         if (isDetectEverything)     return true;
         
         Const_Tags.Tags result = tags.FirstOrDefault(_tag => Const_Tags.TagsMap[_tag] == tag);
+
+        Debug.Log($"Ignoring tag in tags with result {result}");
 
         if (result.Equals(default(Const_Tags.Tags)))    return false;
         else                                            return true;
